5e6c94610b9322bbf30ab83bd0548470f7f796d3
Adrian Kaluzinski <kaluza@gmail.com>
2021-07-27 19:37:41 +0200

    First styled component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>Something's happening</p>
-      <p>Another paragraph</p>
-      <p>Test github integration 2</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
