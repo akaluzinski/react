d0c85ee65c4e998e586422f2cb8afdf1468e1ed8
Adrian Kaluzinski <kaluza@gmail.com>
2021-08-02 19:47:52 +0200

    Display message when no elements left on filtring

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -7,27 +7,34 @@ import { useState } from "react";
 function Expenses(props) {
   const [appliedFilter, setAppliedFilter] = useState({ yearFilter: 2021 });
 
-
-  const filterYear = (expense) => {
-    return expense.date.getFullYear() === appliedFilter.yearFilter;
-  }
+  const filterYear = (expense) =>
+    expense.date.getFullYear() === appliedFilter.yearFilter;
+  const filteredExpenses = props.expenses.filter(filterYear);
 
   const expensesFilterChangeHandler = (selectedYear) => {
     setAppliedFilter(selectedYear);
   };
 
+  const expensesContent = !filteredExpenses.length ? (
+    <p>No expenses found!</p>
+  ) : (
+    filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  );
+
   return (
     <Card className="expenses">
-      <ExpensesFilter onExpensesFilterChange={expensesFilterChangeHandler} />
-      
-      {props.expenses.filter(filterYear).map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesFilter
+        onExpensesFilterChange={expensesFilterChangeHandler}
+        selected={appliedFilter.filterYear}
+      />
+      {expensesContent}
     </Card>
   );
 }
