2c711f1a799e809c52e4a9d48a363fe609ca09b5
Adrian Kaluzinski <kaluza@gmail.com>
2021-08-18 16:12:28 +0200

    Add portal to modal's backdrop and overlay

--- user-form/src/components/UI/error_modal/ErrorModal.js
+++ user-form/src/components/UI/error_modal/ErrorModal.js
@@ -1,23 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 import Card from "../card/Card";
 import Button from "../button/Button";
 import classes from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return (
+    <div className={classes.backdrop} onClick={props.onOkButtonClick}></div>
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onOkButtonClick}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onOkButtonClick}></div>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onOkButtonClick}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      { ReactDOM.createPortal(<Backdrop onOkButtonClick={props.onOkButtonClick}></Backdrop>, document.getElementById("backdrop-root"))}
+      { ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onOkButtonClick={props.onOkButtonClick} ></ModalOverlay>, document.getElementById("overlay-root"))}
+    </>
   );
 };
 
